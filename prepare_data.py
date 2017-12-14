b29df289c60775a905397568ac8659fdb8b9c8b6
FransM22 <f.a.m.mandes@student.tue.nl>
2017-12-14 23:35:16 +0100

    Allow year based selection of values in table

--- prepare_data.py
+++ prepare_data.py
@@ -16,15 +16,14 @@ with open('data/raw/total-final-energy-consumption.csv') as csvfile:
   csv_reader = csv.DictReader(csvfile)
   observations = []
 
-  for row_id, row in enumerate(csv_reader):
-    # TODO - for now limit to only 10 rows
-    if row_id < 10:
-      observation = {
-        'area': row['Area'],
-        'value': row['Value'] 
-      }
-      
-      observations.append(observation)
+  for row in csv_reader:
+    observation = {
+      'area': row['Area'],
+      'year': row['Year'],
+      'value': row['Value']
+    }
+
+    observations.append(observation)
 
   with open('www/data/energy_consumption.json', 'w') as jsonfile:
     json.dump(observations, jsonfile)
