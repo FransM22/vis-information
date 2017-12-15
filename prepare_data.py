dc566b885bf51375a3caebcc0fe836d01c3425a1
FransM22 <f.a.m.mandes@student.tue.nl>
2017-12-15 12:00:01 +0100

    Basic image visualization

--- prepare_data.py
+++ prepare_data.py
@@ -2,17 +2,21 @@
 
 # prepare_data.py requires the following files:
 # data/raw/total-final-energy-consumption.csv ( https://data.london.gov.uk/dataset/total-energy-consumption-borough )
+# data/raw/hxxvxx_006_2015xxx.tif (these are preprocessed data files)
 
 # prepare_data.py processes the raw data files and converts these to json
 # files
 import csv
 import json
 import os
+from PIL import Image
 
 if not os.path.exists('www/data/'):
   os.makedirs('www/data/')
 
 with open('data/raw/total-final-energy-consumption.csv') as csvfile:
+  print('Converting total-final-energy-consumption.csv')
+
   csv_reader = csv.DictReader(csvfile)
   observations = []
 
@@ -28,4 +32,23 @@ with open('data/raw/total-final-energy-consumption.csv') as csvfile:
   with open('www/data/energy_consumption.json', 'w') as jsonfile:
     json.dump(observations, jsonfile)
 
-  print("\nDone")
+print("\nDone")
+
+
+# TODO - this is only a single sample file
+tiffile = Image.open('data/raw/surface_temperature/h17v03_006_2015240204337.tif')
+print('Converting data/raw/surface_temperature/h17v03_006_2015240204337.tif')
+jpgfile = tiffile.convert('RGB')
+jpgfile.save('www/data/h17v03_006_2015240204337.jpg', 'JPEG', quality=90)
+
+with open('www/data/temperature_data.json', 'w') as jsonfile:
+  json.dump({
+    'filename': 'data/h17v03_006_2015240204337.jpg',
+    'north_bound': 59.9958333333333,
+    'south_bound': 50.0041666666667,
+    'east_bound': -0.00649538000934793,
+    'west_bound': -20.0083175793839,
+    'production_datetime': '2015-08-28T20:43:37.000Z'
+  }, jsonfile)
+
+print("\nDone")
