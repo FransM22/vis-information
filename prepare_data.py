fcb3537233c89bad820f403db4bff341cb387149
FransM22 <f.a.m.mandes@student.tue.nl>
2017-12-18 15:47:58 +0100

    Perform raw data processing in python now

--- prepare_data.py
+++ prepare_data.py
@@ -8,10 +8,13 @@
 # files
 import csv
 import json
+import numpy
 import os
 import re
 import shutil
 from datetime import datetime
+from matplotlib import pyplot
+from pyhdf.SD import *
 from PIL import Image
 
 if not os.path.exists('www/data/'):
@@ -37,62 +40,66 @@ with open('data/raw/total-final-energy-consumption.csv') as csvfile:
 
 print("Done")
 
-print('Converting surface temperature files')
-surf_temp_dir = 'data/raw/surface_temperature/'
+print('Converting surface temperature files v2')
+surf_temp_dir = 'data/raw/surface_temperature_v2/'
 png_files = []
 for filename in os.listdir(surf_temp_dir):
-  if filename[-3:] != 'tif':
+  if filename[-3:] != 'hdf':
+    print('Ignoring ' + filename)
     continue
+  hdf_path = surf_temp_dir + filename
+  hdf_file = SD(hdf_path)
 
-  tif_path = surf_temp_dir + filename
-  meta_file_path = surf_temp_dir + filename + '.aux.xml'
+  # Available datasets:
+  #  LST_Day_1km: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 23, 0)
+  #  QC_Day: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 21, 1)
+  #  Day_view_time: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 21, 2)
+  #  Day_view_angl: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 21, 3)
+  #  LST_Night_1km: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 23, 4)
+  #  QC_Night: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 21, 5)
+  #  Night_view_time: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 21, 6)
+  #  Night_view_angl: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 21, 7)
+  #  Emis_31: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 21, 8)
+  #  Emis_32: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 21, 9)
+  #  Clear_day_cov: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 23, 10)
+  #  Clear_night_cov: (('YDim:MODIS_Grid_Daily_1km_LST', 'XDim:MODIS_Grid_Daily_1km_LST'), (1200, 1200), 23, 11)
+  hdf_obj = hdf_file.select('LST_Night_1km')
 
   png_path = 'www/data/' + filename[:-3] + "png"
-  tiffile = Image.open(tif_path)
-  pngfile = tiffile.convert('RGBA')
-  pngfile.save(png_path, 'PNG')
+  data_matrix = numpy.array(hdf_obj.get())
 
-  production_date_line_id, production_date = None, None
-  north_coordinate_line_id, north_coordinate = None, None
-  east_coordinate_line_id, east_coordinate = None, None
-  south_coordinate_line_id, south_coordinate = None, None
-  west_coordinate_line_id, west_coordinate = None, None
+  min_val = min(numpy.array(data_matrix).flatten())
+  max_val = max(numpy.array(data_matrix).flatten())
 
-  if os.path.exists(meta_file_path):
-    with open(meta_file_path) as metafile:
-      lines = metafile.readlines()
-      for line_id, line in enumerate(lines):
-        if re.match(r'\s+OBJECT\s*=\s*PRODUCTIONDATETIME', line):
-          production_date_line_id = line_id + 2
-        if re.match(r'\s+OBJECT\s*=\s*NORTHBOUNDINGCOORDINATE', line):
-          north_coordinate_line_id = line_id + 2
-        if re.match(r'\s+OBJECT\s*=\s*EASTBOUNDINGCOORDINATE', line):
-          east_coordinate_line_id = line_id + 2
-        if re.match(r'\s+OBJECT\s*=\s*SOUTHBOUNDINGCOORDINATE', line):
-          south_coordinate_line_id = line_id + 2
-        if re.match(r'\s+OBJECT\s*=\s*WESTBOUNDINGCOORDINATE', line):
-          west_coordinate_line_id = line_id + 2
+  # low = min(x for x in numpy.array(data_matrix).flatten() if x > min_val)
+  # high = max(x for x in numpy.array(data_matrix).flatten() if x < max_val)
+  low, high = 12000, 14000 # This seems to be an appropriate range for all images
+  print('{} Min: {}, Max {}, Clipping to range ({}, {})'.format(filename, min_val, max_val, low, high))
 
-        if line_id == production_date_line_id:
-          production_date = (line.split('= ')[1]).strip()
-        if line_id == north_coordinate_line_id:
-          north_coordinate = float((line.split('= ')[1]).strip())
-        if line_id == east_coordinate_line_id:
-          east_coordinate = float((line.split('= ')[1]).strip())
-        if line_id == south_coordinate_line_id:
-          south_coordinate = float((line.split('= ')[1]).strip())
-        if line_id == west_coordinate_line_id:
-          west_coordinate = float((line.split('= ')[1]).strip())
+  image_int_matrix = numpy.clip(data_matrix, low, high)
+  image_int_matrix = numpy.divide(numpy.subtract(image_int_matrix, low), high - low)
+  image_int_matrix *= 255
   
-  if production_date is None:
-    production_date = str(datetime.now())
+  img = Image.fromarray(numpy.uint8(image_int_matrix)).convert('RGBA')
 
+  # Set transparency
+  col_img = []
+  for val in img.getdata():
+    intensity = val[0]
+    opacity = 255
+    if intensity == 0:
+      opacity = 0
+    col_img.append((intensity, 0, 0, opacity))
+  img.putdata(col_img)
+  img.save(png_path)
+
+  production_date = str(datetime.now())
   png_files.append({
     'filename': png_path[4:],
-    'north_bound': north_coordinate,
-    'east_bound': east_coordinate,
-    'south_bound': south_coordinate,
-    'west_bound': west_coordinate,
+    'north_bound': None,
+    'east_bound': None,
+    'south_bound': None,
+    'west_bound': None,
     'production_datetime': production_date
   })
 
